517a9d8974cd5f0f02a1e09835e4c18d197605de
Skymen <iskymen@gmail.com>
2016-10-02 01:20:07 +0300

    first commit

---
+++ build/js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.slick').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
+});
+
+//# sourceMappingURL=scripts.js.map
---
+++ src/content.js
@@ -0,0 +1,196 @@
+var data = {
+  data: {
+    menu: [
+      ['Home', ''],
+      ['Features', ''],
+      ['Services', ''],
+      ['News', ''],
+      ['Shop', '']
+    ],
+    slider: [
+      ['Build your business', 'images/01.jpg'],
+      ['Improve your process', 'images/02.jpg'],
+      ['Save more money', 'images/03.jpg']
+    ],
+    advantage: [
+      [
+        '01',
+        'Power Inside',
+        'Cum sociis natoque penatibus et magnis dis parturient montesmus. Pro vel nibh et elit mollis commodo et nec augueique'
+      ],
+      [
+        '02',
+        'New Technologies',
+        'Cum sociis natoque penatibus et magnis dis parturient montesmus. Pro vel nibh et elit mollis commodo et nec augueique'
+      ],
+      [
+        '03',
+        'Fast Support',
+        'Cum sociis natoque penatibus et magnis dis parturient montesmus. Pro vel nibh et elit mollis commodo et nec augueique'
+      ]
+    ],
+    service: [
+      [
+        'Schedule Service',
+        'images/service1.jpg',
+        'http://google.com'
+      ],
+      [
+        'Preventive Maintence',
+        'images/service2.jpg',
+        'http://google.com'
+      ],
+      [
+        'Tire & Wheel Services',
+        'images/service3.jpg',
+        'http://google.com'
+      ],
+      [
+        'Repair Services',
+        'images/service4.jpg',
+        'http://google.com'
+      ]
+    ],
+    news: [
+      [
+        '23',
+        '6',
+        'Advanced Machinery Helps Improve Quality',
+        'Cum sociis natoque penatibus et magnis dis parturient ontesmus. Pro vel nibh et elit mollis commodo et nec augueique Nemo enim ipsam voluptatem quia ptas sit aspernatur samomo enim ipsam voluptatem.',
+        'images/news1.jpg'
+      ],
+      [
+        '21',
+        '3',
+        'Powerful Techniques for Advanced Service',
+        'Cum sociis natoque penatibus et magnis dis parturient ontesmus. Pro vel nibh et elit mollis commodo et nec augueique Nemo enim ipsam voluptatem quia ptas sit aspernatur samomo enim ipsam voluptatem.',
+        'images/news2.jpg'
+      ]
+    ],
+    accordion: [
+      [
+        'Accordion Panel 1',
+        'Cum sociis natoque penatibus et magnis dis parturient mus. Pro vel nibh et elit mollis commodo et nec augueique Nemo enim ipsam voluptatem quia ptas sit aspernatur momo. Cum sociis natoque penatibus et magnis dis parturient ontesmus. Nemo enim ipsam voluptatem quia ptas sit aspernatur samomoPro vel nibh et elit mollis commodo et nec augueique Nemo enim ipsam voluptatem quia ptas sit aspernatur'
+      ],
+      [
+        'Accordion Panel 2',
+        'Cum sociis natoque penatibus et magnis dis parturient mus. Pro vel nibh et elit mollis commodo et nec augueique Nemo enim ipsam voluptatem quia ptas sit aspernatur momo. Cum sociis natoque penatibus et magnis dis parturient ontesmus. Nemo enim ipsam voluptatem quia ptas sit aspernatur samomoPro vel nibh et elit mollis commodo et nec augueique Nemo enim ipsam voluptatem quia ptas sit aspernatur'
+      ],
+      [
+        'Accordion Panel 3',
+        'Cum sociis natoque penatibus et magnis dis parturient mus. Pro vel nibh et elit mollis commodo et nec augueique Nemo enim ipsam voluptatem quia ptas sit aspernatur momo. Cum sociis natoque penatibus et magnis dis parturient ontesmus. Nemo enim ipsam voluptatem quia ptas sit aspernatur samomoPro vel nibh et elit mollis commodo et nec augueique Nemo enim ipsam voluptatem quia ptas sit aspernatur'
+      ],
+      [
+        'Accordion Panel 4',
+        'Cum sociis natoque penatibus et magnis dis parturient mus. Pro vel nibh et elit mollis commodo et nec augueique Nemo enim ipsam voluptatem quia ptas sit aspernatur momo. Cum sociis natoque penatibus et magnis dis parturient ontesmus. Nemo enim ipsam voluptatem quia ptas sit aspernatur samomoPro vel nibh et elit mollis commodo et nec augueique Nemo enim ipsam voluptatem quia ptas sit aspernatur'
+      ]
+    ],
+    partner: [
+      'images/partner1.png',
+      'images/partner2.png',
+      'images/partner3.png',
+      'images/partner4.png',
+      'images/partner5.png',
+      'images/partner6.png'
+    ],
+    socials: {
+      title: 'Socials',
+      content: [
+        [
+          'Facebook',
+          'https://facebook.com'
+        ],
+        [
+          'Flickr',
+          'https://facebook.com'
+        ],
+        [
+          'Twitter',
+          'https://facebook.com'
+        ],
+        [
+          'Vimeo',
+          'https://facebook.com'
+        ]
+      ]
+    },
+    links: {
+      title: 'Links',
+      content: [
+        [
+          'How to Write a Blog',
+          'https://facebook.com'
+        ],
+        [
+          'Create a Blog in 4 Steps',
+          'https://facebook.com'
+        ],
+        [
+          '15 Tips to Increase Traffic',
+          'https://facebook.com'
+        ],
+        [
+          '20 Blog Post Ideas',
+          'https://facebook.com'
+        ]
+      ]
+    },
+    blogroll: {
+      title: 'Blogroll',
+      content: [
+        [
+          'Blogs Blog',
+          'https://facebook.com'
+        ],
+        [
+          'Roll Up Roll Up',
+          'https://facebook.com'
+        ],
+        [
+          'Blog Free',
+          'https://facebook.com'
+        ],
+        [
+          'Wordpress Blog',
+          'https://facebook.com'
+        ]
+      ]
+    },
+    about: {
+      title: 'About us',
+      content: [
+        [
+          'Agriculture',
+          'https://facebook.com'
+        ],
+        [
+          'Harvest',
+          'https://facebook.com'
+        ],
+        [
+          'Company news',
+          'https://facebook.com'
+        ],
+        [
+          'Investor relations',
+          'https://facebook.com'
+        ]
+      ]
+    },
+    footer: {
+      address: 'BuildHomeLtd.  Street Name 123   23 45 Cityname',
+      phone: '+123 900 123 456',
+      email: 'info@buildhome.web',
+      copyright: '© 2014 Build Home'
+    },
+    follow: {
+      facebook: 'https://facebook.com',
+      twitter: 'https://facebook.com',
+      rss: 'https://facebook.com',
+      linkedin: 'https://facebook.com',
+      skype: 'https://facebook.com'
+    }
+  }
+};
+
+module.exports = data;
